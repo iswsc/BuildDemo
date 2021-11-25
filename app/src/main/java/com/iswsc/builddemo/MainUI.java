d1e28f838c8e2f5d4e7ab5cd5213fe639b216e6b
Jacen <jacen@iswsc.com>
2021-11-25 10:43:54 +0800

    init

---
+++ app/src/main/java/com/iswsc/builddemo/MainUI.java
@@ -0,0 +1,11 @@
+package com.iswsc.builddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author jacen
+ * @date 2021/11/25 10:28 上午
+ * @email jacen@iswsc.com
+ */
+public class MainUI extends AppCompatActivity {
+}
